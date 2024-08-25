aec13c342c24e20f176881e3dadfec633fe40539
OLADIMEJI988 <oladimejimax49@gmail.com>
2024-08-25 12:39:56 +0100

    updated functionality

--- dist/cart.js
+++ dist/cart.js
@@ -3,24 +3,53 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleContainers = document.querySelectorAll(".card");
 
   toggleContainers.forEach((container) => {
-    const button = container.querySelector(".toggleButton");
+    const addToCartBtn = container.querySelector(".toggleButton");
     const content = container.querySelector(".content");
-    const active = container.querySelector(".active");
     const selected = container.querySelector(".selected");
-    button.addEventListener("click", function () {
-      // Toggle the visibility of the content
-      content.classList.remove("hidden");
-      content.classList.add("flex");
-      button.classList.add("hidden");
-      selected.classList.add("border-[3px]", "border-solid", "img-border-color");
-    });
 
-    active.addEventListener("click", function () {
-        // Toggle the visibility of the content
-      content.classList.remove("flex");
-      content.classList.add("hidden");
-      button.classList.remove("hidden");
-      selected.classList.remove("border-[3px]", "border-solid", "img-border-color");
-    });
+    addToCartBtn.addEventListener("click", incrementAmount);
+
+    // --- adding to cart ---
+    const decrementButton = container.querySelector("#decrement");
+    const incrementButton = container.querySelector("#increment");
+    const amountDisplay = container.querySelector("#amount");
+    const orderButton = container.querySelector(".order");
+
+    let amount = Number(amountDisplay.textContent, 10);
+
+    function decrementAmount() {
+      amount -= 1;
+      updateUI();
+    }
+
+    function incrementAmount() {
+      amount += 1;
+      updateUI();
+    }
+
+    decrementButton.addEventListener("click", decrementAmount);
+    incrementButton.addEventListener("click", incrementAmount);
+
+    function updateUI() {
+      if (amount > 0) {
+        content.classList.remove("hidden");
+        content.classList.add("flex");
+        addToCartBtn.classList.add("hidden");
+        selected.classList.add(
+          "border-[3px]",
+          "border-solid",
+          "img-border-color"
+        );
+      } else {
+        orderButton.classList.add("hidden");
+        addToCartBtn.classList.remove("hidden");
+        selected.classList.remove(
+          "border-[3px]",
+          "border-solid",
+          "img-border-color"
+        );
+      }
+      amountDisplay.textContent = amount;
+    }
   });
 });
