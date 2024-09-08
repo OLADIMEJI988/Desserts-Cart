47adde78ab19c19eadabecf03f0d144f76a714f0
OLADIMEJI988 <oladimejimax49@gmail.com>
2024-09-08 14:46:13 +0100

    view updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     screens: {
+      xs: "400px",
       sm: "640px",
       tablet:"750px",
       md:"860px",
       lg: "967px",
       xl: "1024px",
+      xxl: "1280px",
     },
     extend: {},
   },
